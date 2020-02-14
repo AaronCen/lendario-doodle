e555e92e55c919bdc192559b03b8d61bad74bbe0
AaronCen <61056069+AaronCen@users.noreply.github.com>
2020-02-14 10:19:59 -0600

    Add files via upload
    Programa de Métodos Numéricos

---
+++ MetodosNumericos/src/metodosnumericos/MetodosNumericos.java
@@ -0,0 +1,13 @@
+
+package metodosnumericos;
+
+
+public class MetodosNumericos {
+
+    
+    public static void main(String[] args) {
+        frmPrincipal ventana = new frmPrincipal();
+        ventana.setVisible(true);
+    }
+    
+}
